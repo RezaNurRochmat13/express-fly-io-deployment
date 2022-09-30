cdf9754c8960dfc53aa51026d31d5dc5ed1db641
Reja <rezanurrochmat@gmail.com>
2022-09-30 10:50:26 +0700

    done deploy to fly

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 8000
+const PORT = process.env.PORT || 8080
 
 app.get("/", (request, response) => {
     response.json({message: "Service up"});
